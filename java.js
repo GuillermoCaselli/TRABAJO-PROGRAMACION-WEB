a7f0b6d18d1dd0e3ed8503936e94c97d84ff7355
GuillermoCaselli <gcaselliseippel@itba.edu.ar>
2022-11-18 12:15:32 -0300

    PUSH

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-let productos=[]
+let arrayProducto =[]
+let Total = 0
+function sumaProductos(Valoritem, Producto) {
+    arrayProducto.push(Producto)
+  Total = Total + Valoritem
+  document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + Total; 
+  console.log(arrayProducto);
+  document.getElementById("valorProducto").value = Total
+  document.getElementById("listaProductos").value=arrayProducto
+  return Total}
+
+function Limpiar() {
+  Total=0
+  document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + 0; }
